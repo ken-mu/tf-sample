8202d4db82a2565eff94c17def98dab48497593c
ken-mu <gas.010bl@gmail.com>
2019-11-10 21:17:56 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -127,13 +127,7 @@ resource "aws_elb" "bar" {
 
 data "aws_ami" "amazon-linux-2" {
  most_recent = true
-
-
- filter {
-   name   = "owner-alias"
-   values = ["amazon"]
- }
-
+ owners           = ["amazon"]
 
  filter {
    name   = "name"
