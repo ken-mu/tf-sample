b95ce997493bc18d9aece0a5c673902025ab222b
ken-mu <gas.010bl@gmail.com>
2019-11-10 20:30:35 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -107,8 +107,8 @@ resource "aws_elb" "bar" {
   }
   
   health_check {
-    healthy_threshold   = 1
-    unhealthy_threshold = 1
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
     timeout             = 3
     target              = "HTTP:80/"
     interval            = 30
